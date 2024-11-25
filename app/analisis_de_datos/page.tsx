f94a03bd3304f11b38694b9bf775c795698afa62
vextroyer <victormanuel.vena@gmail.com>
2024-11-25 10:39:11 -0500

    Render arguments based on selected query

--- app/analisis_de_datos/page.tsx
+++ app/analisis_de_datos/page.tsx
@@ -24,16 +24,26 @@ export default function AnalisisDeDatos(){
 }
 
 function QueryItem(){
+	const options = ["A","B","C"]
+	const [option,setOption] = useState("A")
+
 	return(
 		<div className="flex flex-col bg-gray-700">
 				<div className="flex flex-row">
-					<p className="p-2 m-4 inline bg-red-700">Dropdown list for selecting query</p>
+					<select className="p-2 m-4 inline bg-red-700" value={option} onChange={e => setOption(e.target.value)}>
+						{options.map(option => (
+							<option value={option}>{option}</option>
+						))}
+					</select>
 				</div>
 				
 				<div className="p-2 m-4 flex flex-row bg-red-700 gap-4">
-					<p className="p-2 bg-orange-300">Set first parameter</p>
+					{option=="A" && <p className="p-2 bg-orange-300">Elegiste la opcion A</p>}
+					{option=="B" && <p className="p-2 bg-orange-300">Elegiste la opcion B</p>}
+					{option=="C" && <p className="p-2 bg-orange-300">Elegiste la opcion C</p>}
+					{/*<p className="p-2 bg-orange-300">Set first parameter</p>
 					<p className="p-2 bg-orange-300">Set second parameter</p>
-					<p className="p-2 bg-orange-300">Set third parameter</p>
+					<p className="p-2 bg-orange-300">Set third parameter</p>*/}
 				</div>
 				
 				<div className="m-4 bg-yellow-700">
