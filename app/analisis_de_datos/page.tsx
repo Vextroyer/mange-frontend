69a7b0a8819941b566e3f35067354d9bf2f7dae3
vextroyer <victormanuel.vena@gmail.com>
2024-11-25 00:14:39 -0500

    Setting up data analisis page

---
+++ app/analisis_de_datos/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+export default function AnalisisDeDatos(){
+	return (
+		<div className="flex flex-col m-4 h-screen flex-row">
+			<button onClick={QueryItem}>
+				New
+			</button>
+		</div>
+	)
+}
+
+function QueryItem(){
+	return(
+		<div className="flex flex-col bg-gray-700">
+				<div className="flex flex-row">
+					<p className="p-2 m-4 inline bg-red-700">Dropdown list for selecting query</p>
+				</div>
+				
+				<div className="p-2 m-4 flex flex-row bg-red-700 gap-4">
+					<p className="p-2 bg-orange-300">Set first parameter</p>
+					<p className="p-2 bg-orange-300">Set second parameter</p>
+					<p className="p-2 bg-orange-300">Set third parameter</p>
+				</div>
+				
+				<div className="m-4 bg-yellow-700">
+					
+					<div className="flex flex-row">
+						<p className="p-2 m-4 bg-red-700">Exportar</p>
+					</div>
+					
+					Graficos
+				</div>
+		</div>
+	)
+}
