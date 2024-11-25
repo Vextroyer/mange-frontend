facf7544a35e4fdb4972c234bf3042bb48ee5ce7
vextroyer <victormanuel.vena@gmail.com>
2024-11-24 22:18:52 -0500

    Link login page with home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+import Link from 'next/link'
+
+export default function Login() {
   return (
 	<main>
 		<form className="bg-white text-black block p-4 w-96 rounded-lg ">
@@ -9,9 +11,11 @@ export default function Home() {
 			<div className="relative">
 				<input className="p-4 my-2 mx-0 outline-0 border border-solid rounded-lg border-gray-300 text-sm w-80" placeholder="Enter password" type="password"/>
 			</div>
+			<Link href="/home">
 			<button className="block px-5 py-3 my-2 bg-blue-600 border rounded-lg text-white text-sm font-medium uppercase text-center w-80" type="submit">
 				Sign in
 			</button>
+			</Link>
 		</form>
 	</main>
   );
